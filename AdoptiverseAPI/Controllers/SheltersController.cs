95f3e86ecf7fce921f67e08e8b0a7fec171430de
Eli-J-Paris <Eli.J.Paris@gmail.com>
2023-08-29 15:56:23 -0600

    Added crud and tests for shelter

--- AdoptiverseAPI/Controllers/SheltersController.cs
+++ AdoptiverseAPI/Controllers/SheltersController.cs
@@ -1,4 +1,5 @@
 using AdoptiverseAPI.DataAccess;
+using AdoptiverseAPI.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AdoptiverseAPI.Controllers
@@ -18,6 +19,45 @@ namespace AdoptiverseAPI.Controllers
         public ActionResult GetShelters()
         {
             var shelters = _context.Shelters;
+            return new JsonResult(shelters);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult ReturnShelter(int id)
+        {
+            var shelter = _context.Shelters.Find(id);
+
+            return new JsonResult(shelter);
+
+        }
+
+        [HttpPost]
+        public ActionResult CreateShelter(Shelter shelter)
+        {
+            _context.Shelters.Add(shelter);
+            _context.SaveChanges();
+
+            return new JsonResult(shelter);
+        }
+
+        [HttpPut("{id}")]
+        public ActionResult UpdateShelter(int id, Shelter shelter)
+        {
+            shelter.Id = id;
+            _context.Shelters.Update(shelter);
+            _context.SaveChanges();
+            Response.StatusCode = 204;
+            return new JsonResult(shelter);
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult DeleteShelter(int id)
+        {
+            var shelters = _context.Shelters;
+            var shelter = shelters.Find(id);
+            _context.Shelters.Remove(shelter);
+            _context.SaveChanges();
+
             return new JsonResult(shelters);
         }
     }
--- AdoptiverseEndpointTests/ShelterCrudEndpointTests.cs
+++ AdoptiverseEndpointTests/ShelterCrudEndpointTests.cs
@@ -5,6 +5,7 @@ using Newtonsoft.Json.Serialization;
 using Newtonsoft.Json;
 using AdoptiverseAPI.Models;
 using static System.Reflection.Metadata.BlobBuilder;
+using System.Text;
 
 namespace AdoptiverseEndpointTests
 {
@@ -31,7 +32,7 @@ namespace AdoptiverseEndpointTests
         {
             DefaultContractResolver contractResolver = new DefaultContractResolver
             {
-                NamingStrategy = new SnakeCaseNamingStrategy()
+                NamingStrategy = new CamelCaseNamingStrategy()
             };
 
             string json = JsonConvert.SerializeObject(obj, new JsonSerializerSettings
@@ -65,6 +66,72 @@ namespace AdoptiverseEndpointTests
             response.EnsureSuccessStatusCode();
             Assert.Equal(expected, content);
 
+        }
+
+        [Fact]
+        public async void ReturnShelter_ReturnsSingleShelter()
+        {
+            HttpClient client = _factory.CreateClient();
+            AdoptiverseApiContext context = GetDbContext();
+
+            Shelter shelter1 = new Shelter { Name = "Smart Friend League", Rank = 3, City = "denver" };
+            Shelter shelter2 = new Shelter { Name = "BigBrain Friend League", Rank = 9, City = "Aurora" };
+
+            List<Shelter> shelters = new() { shelter1, shelter2 };
+            context.AddRange(shelters);
+            context.SaveChanges();
+
+            //act
+            HttpResponseMessage response = await client.GetAsync($"/api/shelters/{shelter1.Id}");
+            string content = await response.Content.ReadAsStringAsync();
+
+            string expected = ObjectToJson(shelter1);
+
+            //Assert
+            response.EnsureSuccessStatusCode();
+            Assert.DoesNotContain(ObjectToJson(shelter2), content);
+            Assert.Equal(expected, content);
+
+        }
+
+        [Fact]
+        public async void UpdateShelter_UpdatesExsistingshelter()
+        {
+            HttpClient client = _factory.CreateClient();
+            AdoptiverseApiContext context = GetDbContext();
+
+            Shelter shelter1 = new Shelter { Name = "Smart Friend League", Rank = 3, City = "denver" };
+            context.Add(shelter1);
+            context.SaveChanges();
+
+            var jsonString = "{\"CreatedAt\": \"2023-08-29T12:00:00.000Z\", \"UpdatedAt\": \"2023-08-29T12:05:00.000Z\", \"HasFosterProgram\": true, \"Rank\": 1, \"City\": \"San Francisco\", \"Name\": \"Happy Paws\"}";
+            var requestContent = new StringContent(jsonString, Encoding.UTF8, "application/json");
+            var response = await client.PutAsync("/api/shelters/1", requestContent);
+
+            context.ChangeTracker.Clear();
+
+            Assert.Equal(204, (int)response.StatusCode);
+            Assert.Equal("Happy Paws", context.Shelters.Find(1).Name);
+
+
+        }
+
+        [Fact]
+        public async void DeleteShelter_DeletesExsistingShelter()
+        {
+            HttpClient client = _factory.CreateClient();
+            AdoptiverseApiContext context = GetDbContext();
+
+            Shelter shelter1 = new Shelter { Name = "Smart Friend League", Rank = 3, City = "denver" };
+            context.Add(shelter1);
+            context.SaveChanges();
+
+
+            var response = await client.DeleteAsync("/api/shelters/1");
+            var content = await response.Content.ReadAsStringAsync();
+
+            //Assert.Equal(204, (int)response.StatusCode);
+            Assert.DoesNotContain("denver", content);
         }
     }
 }
