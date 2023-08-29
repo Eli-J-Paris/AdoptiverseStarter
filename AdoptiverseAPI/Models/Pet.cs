0c81108ccb559536e6cc1897e80a09e4138dac78
Eli-J-Paris <Eli.J.Paris@gmail.com>
2023-08-29 14:10:37 -0600

    added models

---
+++ AdoptiverseAPI/Models/Pet.cs
@@ -0,0 +1,14 @@
+﻿namespace AdoptiverseAPI.Models
+{
+    public class Pet
+    {
+        public int Id { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        public bool IsAdoptable{ get; set; }
+        public int Age { get; set; }
+        public string Breed { get; set; }
+        public string Name { get; set; }
+        public Shelter Shelter { get; set; }
+    }
+}
